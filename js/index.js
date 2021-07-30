309f55b134b371fe3d4cd677d2dd8f309cb9c945
Moustapha Kachab <moustaphakachab@esp.sn>
2021-07-30 05:49:42 +0000

    V1 of the website

---
+++ js/index.js
@@ -0,0 +1,26 @@
+document.querySelector("#pop>img").addEventListener("click", () => {
+    document.getElementById("pop").style.bottom = "-100%";
+});
+
+document.querySelector("#hamburger-menu").addEventListener("click", () => {
+    document.querySelector("#mobile-head>nav").style.left = "0%";
+});
+
+document.querySelector("main").addEventListener("click", () => {
+    document.querySelector("#mobile-head>nav").style.left = "-100%";
+});
+
+document.querySelector("#exit-mobile").addEventListener("click", () => {
+    document.querySelector("#mobile-head>nav").style.left = "-100%";
+});
+
+const header = document.getElementById("head");
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 500){
+        header.classList.add("head-scroll-up");
+    }
+    else{
+        header.classList.remove("head-scroll-up");
+    }
+})
